985ad4bb27996acb6e173471778e3525296662c5
sarvajeet-hue <sarvajeet.1923en1095@kiet.edu>
2025-04-07 12:16:54 +0530

    updated navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,42 @@
 import Navbar from "./components/common/Navbar";
-import cursor_img from './assets/gradient-hero-prerender.avif'
-import curor_temp from './assets/watch-demo.avif'
+import cursor_img from "./assets/gradient-hero-prerender.avif";
+import curor_temp from "./assets/watch-demo.avif";
 
 function App() {
   return (
     <>
+      {/* <div className="container">
+        <Navbar />
+      </div>
 
-<div className="container">
-<Navbar/> 
-</div>
+      <div className="banner">
+        {<img src={cursor_img} className="object-cover rounded-lg w-full" />}
 
-<div className="banner">
-{ <img src={cursor_img} className="object-cover rounded-lg w-full" /> }
-
-    <div className="bnnertext">
-    <h1>The First Code Editor</h1>
-    <p>Built to make you extraordinarily productive, Cursor is the best way to code with AI.</p>
-    <div className="button">
-        <span>Download</span>
-        <span>ALL Download</span>
-    </div>
-    <img src={curor_temp} className="object-cover rounded-lg w-full" />
-
-    </div>
-</div>
-
-
-<div className="banner">
-<p> kj kj klj kl jkl j klj kl jkl j kl jkl j klj kl jkl h jhgh gf h</p>
-</div>
-
-
-      {/* <div className="min-h-screen bg-black text-white p-4 sm:p-6 lg:p-8">
-        <div className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 relative">
-
-         
-          <div className="w-full flex flex-col md:flex-row p-4 sm:p-6 lg:p-8 ">
-            <section className=" w-full">
-                <div className="md:shrink-0 relative mt-[50px]">
-                 
-                  <h1 className="text-center text-white font-bold text-lg sm:text-2xl lg:text-4xl absolute inset-0">The First Code Editor</h1>
-                </div>
-                
-            </section>  
+        <div className="bnnertext">
+          <h1>The First Code Editor</h1>
+          <p>
+            Built to make you extraordinarily productive, Cursor is the best way
+            to code with AI.
+          </p>
+          <div className="button">
+            <span>Download</span>
+            <span>ALL Download</span>
           </div>
+          <img src={curor_temp} className="object-cover rounded-lg w-full" />
         </div>
+      </div>
+
+      <div className="banner">
+        <p> kj kj klj kl jkl j klj kl jkl j kl jkl j klj kl jkl h jhgh gf h</p>
       </div> */}
+
+        <div>
+          <Navbar/>
+        </div>
+
+
+
+
     </>
   );
 }
--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,56 +1,37 @@
+import { Button } from "../ui/button";
+import menubar_icon from "../../assets/menu-bar.png";
+
 const Navbar = () => {
   return (
-<div>
+    <div className="fixed w-full z-50 left-0 shadow-md ">
+      <div className="max-w-7xl mx-auto p-4 bg-black">
+        <nav className="text-white flex items-center justify-between rounded-lg px-4">
+          <div>
+            <img src="" alt="" />
+            <h1 className="font-bold text-2xl">CURSOR</h1>
+          </div>
 
-    <nav className="headercolor">
-        <div className="headercolors flex items-center justify-between">
-            <div className="text-white">
-                Cursor
-            </div>
-          <button
-            type="button"
-            className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-white focus:outline-hidden focus:ring-inset"
-            aria-controls="mobile-menu"
-            aria-expanded="false"
-          >
-            <span className="absolute -inset-0.5"></span>
-            <span className="sr-only">Open main menu</span>
+          <div className="hidden lg:flex items-center gap-3 font-semibold">
+            <p>PRICING</p>
+            <p>FEATURES</p>
+            <p>ENTERPRISE</p>
+            <p>BLOG</p>
+            <p>FORUM</p>
+            <p>CAREERS</p>
+          </div>
 
-            <svg
-              className="block size-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              aria-hidden="true"
-              data-slot="icon"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-              />
-            </svg>
+          <div className="hidden lg:flex items-center gap-3">
+            <Button>Sign In</Button>
+            <Button variant="secondary">Download</Button>
+          </div>
 
-            <svg
-              className="hidden size-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              aria-hidden="true"
-              data-slot="icon"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M6 18 18 6M6 6l12 12"
-              />
-            </svg>
-          </button>
-        </div>
-    </nav>
-
+          <div className="lg:hidden">
+            <Button>
+              <img src={menubar_icon} alt="" className="w-5" />
+            </Button>
+          </div>
+        </nav>
+      </div>
     </div>
   );
 };
