81152cec2e6cc016bb8a28afdaf0390bce613b96
sarvajeet-hue <sarvajeet.1923en1095@kiet.edu>
2025-04-07 12:31:15 +0530

    updated navbar

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,13 +1,14 @@
 import { Button } from "../ui/button";
 import menubar_icon from "../../assets/menu-bar.png";
+import cursor_icon from '../../assets/cursor_icon dark.png'
 
 const Navbar = () => {
   return (
-    <div className="fixed w-full z-50 left-0 shadow-md ">
+    <div className="fixed w-full top-[20px] z-50 left-0 shadow-md ">
       <div className="max-w-7xl mx-auto p-4 bg-black">
         <nav className="text-white flex items-center justify-between rounded-lg px-4">
-          <div>
-            <img src="" alt="" />
+          <div className="flex items-center gap-3 group">
+            <img src={cursor_icon} alt="" className="w-[30px]  transition-transform duration-500 group-hover:rotate-[360deg] " />
             <h1 className="font-bold text-2xl">CURSOR</h1>
           </div>
 
